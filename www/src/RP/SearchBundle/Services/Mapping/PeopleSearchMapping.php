8866e8f62203ef4c16f9e0b660e16e3c0181cdc6
E Leshanu <leshanu@russianplace.com>
2017-01-24 14:35:58 +0300

    Изменил порядок поиска релевантности

--- www/src/RP/SearchBundle/Services/Mapping/PeopleSearchMapping.php
+++ www/src/RP/SearchBundle/Services/Mapping/PeopleSearchMapping.php
@@ -324,7 +324,7 @@ abstract class PeopleSearchMapping extends AbstractSearchMapping
         }
 
         return [
-            $conditionFactory->getDisMaxQuery(array_merge([
+            /*$conditionFactory->getDisMaxQuery(array_merge([
                     $conditionFactory->getMultiMatchQuery()
                                      ->setFields(array_merge(
                                          self::getMultiMatchQuerySearchFields(),
@@ -336,6 +336,27 @@ abstract class PeopleSearchMapping extends AbstractSearchMapping
                 ],$prefixWildCardByTags, $prefixWildCardByName,[
                     $conditionFactory->getFieldQuery(self::getMorphologyQuerySearchFields(), $queryString, true, 0.5)
                 ]
+            ))*/
+            $conditionFactory->getDisMaxQuery(array_merge(
+                [
+                    $conditionFactory->getMultiMatchQuery()
+                                     ->setFields(self::getMultiMatchQuerySearchFields())
+                                     ->setQuery($queryString)
+                                     ->setOperator(MultiMatch::OPERATOR_OR)
+                                     ->setType(MultiMatch::TYPE_BEST_FIELDS)
+                ],
+                [
+                    $conditionFactory->getMultiMatchQuery()
+                                     ->setFields(self::getMultiSubMatchQuerySearchFields())
+                                     ->setQuery($queryString)
+                                     ->setOperator(MultiMatch::OPERATOR_OR)
+                                     ->setType(MultiMatch::TYPE_BEST_FIELDS)
+                ],
+                $prefixWildCardByName,
+                [
+                    $conditionFactory->getFieldQuery(self::getMorphologyQuerySearchFields(), $queryString, true, 0.5)
+                ],
+                $prefixWildCardByTags
             ))
         ];
     }

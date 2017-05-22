009ec82439bd7305f5d1aba8c6ccaaa7d29623d8
E Leshanu <leshanu@russianplace.com>
2017-05-22 19:08:26 +0300

    Реализовал методы вывода постов ленты и вывод ленты по ID

--- www/src/RP/SearchBundle/Controller/SearchNewsFeedController.php
+++ www/src/RP/SearchBundle/Controller/SearchNewsFeedController.php
@@ -7,6 +7,8 @@ namespace RP\SearchBundle\Controller;
 
 use Common\Core\Controller\ApiController;
 use Common\Core\Constants\RequestConstant;
+use RP\SearchBundle\Services\Mapping\PostSearchMapping;
+use RP\SearchBundle\Services\NewsFeedSearchService;
 use RP\SearchBundle\Services\Traits\PeopleServiceTrait;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,30 +22,83 @@ class SearchNewsFeedController extends ApiController
     /**
      * Получаем ленту по ID стены
      *
-     *
      * @param Request $request
      * @param string $wallId
-     *
      * @return Response
      */
     public function getNewsFeedPostsAction(Request $request, $wallId)
     {
-        //$userEventsGroup = $this->getUserEventsGroups();
+
+        try {
+            /** @var ID пользователя */
+            $userId = $this->getRequestUserId();
+            $version = $request->get(RequestConstant::VERSION_PARAM, RequestConstant::DEFAULT_VERSION);
+
+            /** @var NewsFeedSearchService */
+            $postSearchService = $this->getNewsFeedSearchService();
+            $posts = $postSearchService->searchPostsByWallId($userId, $wallId, $this->getSkip(), $this->getCount());
+
+            if (empty($posts)) {
+                return $this->_handleViewWithData([]);
+            }
+
+            if ($version == RequestConstant::DEFAULT_VERSION) {
+                return $this->_handleViewWithData($posts[PostSearchMapping::CONTEXT]);
+            }
+
+            return $this->_handleViewWithData([
+                'info'       => $postSearchService->getTotalHits(),
+                'pagination' => $postSearchService->getPaginationAdapter(
+                    $this->getSkip(),
+                    $this->getCount()
+                ),
+                'items'      => $posts[PostSearchMapping::CONTEXT],
+            ]);
+
+        } catch (SearchServiceException $e) {
+            return $this->_handleViewWithError($e);
+        } catch (\HttpResponseException $e) {
+            return $this->_handleViewWithError($e);
+        }
     }
 
+    /**
+     * Получаем пост по его ID
+     *
+     * @param Request $request
+     * @param string postId
+     * @return Response
+     */
+    public function getNewsFeedPostByIdAction(Request $request, $postId)
+    {
+        try {
+            $userId = $this->getRequestUserId();
+            $postSearchService = $this->getNewsFeedSearchService();
+
+            $post = $postSearchService->searchPostById($userId, $postId);
+
+            if (empty($post)) {
+                return $this->_handleViewWithData(new \stdClass());
+            }
+
+            return $this->_handleViewWithData($post);
+        } catch (SearchServiceException $e) {
+            return $this->_handleViewWithError($e);
+        } catch (\HttpResponseException $e) {
+            return $this->_handleViewWithError($e);
+        }
+    }
 
     /**
      * Получаем пользовательские события
      * если не задан userId то смотрим свои события
      * если он задан то мы смотрим события другого профиля (в случае если он разрешает это делать)
      *
-     *
      * @param Request $request
      * @param string $userId
-     *
      * @return Response
      */
-    public function getNewsFeedUserEventsAction(Request $request, $userId)
+    public function getNewsFeedUserEventsAction(Request $request, $userId = null)
     {
 
     }

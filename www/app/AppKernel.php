b362eaa231943f3e67d97ee763b89fd732680ea2
E Leshanu <leshanu@russianplace.com>
2017-06-09 16:02:11 +0300

    Убрал редис репозиторий из поиска

--- www/app/AppKernel.php
+++ www/app/AppKernel.php
@@ -18,7 +18,7 @@ class AppKernel extends Kernel
             // сторонние бандлы
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
-            new Snc\RedisBundle\SncRedisBundle(),
+            //new Snc\RedisBundle\SncRedisBundle(),
             new FOS\ElasticaBundle\FOSElasticaBundle(),
 
             // personal Bundles,

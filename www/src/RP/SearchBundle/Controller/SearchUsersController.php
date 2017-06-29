0628e77176277225b080300401ea502e1900f54f
Лешану Евгений <leshanu@russianplace.com>
2017-06-29 12:34:15 +0300

    Merge branch 'RP-2811/extra-auth' into 'develop'
    RP-2811 добавлен метод получения профиля текущего пользователя (по токену)
    
    See merge request !107

--- www/src/RP/SearchBundle/Controller/SearchUsersController.php
+++ www/src/RP/SearchBundle/Controller/SearchUsersController.php
@@ -271,12 +271,29 @@ class SearchUsersController extends ApiController
      */
     public function searchUsersByIdAction(Request $request, $userId)
     {
-        $peopleSearchService = $this->getPeopleSearchService();
+        $version = $request->get(RequestConstant::VERSION_PARAM, RequestConstant::NEW_DEFAULT_VERSION);
+        $targetUserId = $request->get(RequestConstant::TARGET_USER_ID_PARAM, $this->getRequestUserId());
+
+        return $this->searchUserById($targetUserId, $version);
+    }
+
+    /**
+     * Поиск пользователя по токену авторизации
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request Объект запроса
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function searchCurrentUserAction(Request $request)
+    {
         $version = $request->get(RequestConstant::VERSION_PARAM, RequestConstant::NEW_DEFAULT_VERSION);
 
+        return $this->searchUserById($this->getRequestUserId(), $version);
+    }
+
+    private function searchUserById($targetUserId, $version = RequestConstant::NEW_DEFAULT_VERSION)
+    {
         try {
-            //$targetUserId = $request->get(RequestConstant::TARGET_USER_ID_PARAM, $userId);
-            $targetUserId = $request->get(RequestConstant::TARGET_USER_ID_PARAM, $this->getRequestUserId());
+            $peopleSearchService = $this->getPeopleSearchService();
 
             $userContext = null;
             if ($targetUserId != $this->getRequestUserId()) {
